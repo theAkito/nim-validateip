298102906461e3deca36e42a20525d6ca03b9882
Akito <akito.kitsune@protonmail.com>
2019-06-25 22:02:03 +0200

    Improvements!

--- validateip/ip4.nim
+++ validateip/ip4.nim
@@ -3,64 +3,44 @@ import strutils
 type   
   Ip4a* = object
     Sub0: aRange
-    Sub1: baseRange
-    Sub2: baseRange
-    Sub3: baseRange
+    Sub1: BaseRange
+    Sub2: BaseRange
+    Sub3: BaseRange
 
   Ip4b* = object
     Sub0: bRange
-    Sub1: baseRange
-    Sub2: baseRange
-    Sub3: baseRange
+    Sub1: BaseRange
+    Sub2: BaseRange
+    Sub3: BaseRange
 
   Ip4c* = object
     Sub0: cRange
-    Sub1: baseRange
-    Sub2: baseRange
-    Sub3: baseRange 
+    Sub1: BaseRange
+    Sub2: BaseRange
+    Sub3: BaseRange 
 
   Ip4d* = object
     Sub0: dRange
-    Sub1: baseRange
-    Sub2: baseRange
-    Sub3: baseRange
+    Sub1: BaseRange
+    Sub2: BaseRange
+    Sub3: BaseRange
 
   Ip4all* = object
-    Sub0: baseRange
-    Sub1: baseRange
-    Sub2: baseRange
-    Sub3: baseRange
+    Sub0: BaseRange
+    Sub1: BaseRange
+    Sub2: BaseRange
+    Sub3: BaseRange
   
   
   aRange = range[0..127]
   bRange = range[128..191]
   cRange = range[192..223]
   dRange = range[224..239]
-  baseRange = range[0..255]
-
-proc baseRanger(partialAddress: seq): bool =
-  try:
-    let
-      partAddr1: baseRange = partialAddress[0].parseInt
-      partAddr2: baseRange = partialAddress[1].parseInt
-      partAddr3: baseRange = partialAddress[2].parseInt
-    return true
-  except:
-    return false
-
-proc isIp4(address: string): bool =
-  let splitAddress = address.split('.')
-  if splitAddress.len != 4:
-    return false
-  else:
-    try:
-      let
-        partAddr0: baseRange = splitAddress[0].parseInt
-        partAddr1: baseRange = splitAddress[1].parseInt
-        partAddr2: baseRange = splitAddress[2].parseInt
-        partAddr3: baseRange = splitAddress[3].parseInt
-    except:
-      return false
+  BaseRange = range[0..255]
+  ClassRange = range['A'..'D']
+  
+  ClassNick = enum
+    large, medium, small, multicast
 
 proc getPrime(address: string): int =
   let
@@ -68,12 +48,36 @@ proc getPrime(address: string): int =
     prime = splitAddress[0].parseInt
   return prime
 
-proc validIp4c(address: string): bool =
+proc isIp4*(address: string): bool =
+  let splitAddress = address.split('.')
+  if splitAddress.len != 4:
+    return false
+  else:
+    try:
+      let
+        partAddr0: BaseRange = splitAddress[0].parseInt
+        partAddr1: BaseRange = splitAddress[1].parseInt
+        partAddr2: BaseRange = splitAddress[2].parseInt
+        partAddr3: BaseRange = splitAddress[3].parseInt
+    except:
+      return false
+
+proc validIp4*(address: string, class: ClassRange): bool =
   if not address.isIp4:
     return false
   else:
     try:
-      let validPrime: cRange = address.getPrime
+      case class.toLowerAscii
+      of 'a':
+        let validPrime: aRange = address.getPrime
+      of 'b':
+        let validPrime: bRange = address.getPrime
+      of 'c':
+        let validPrime: cRange = address.getPrime
+      of 'd':
+        let validPrime: dRange = address.getPrime
+      else:
+        "Invalid IPv4 address class.".quit
       return true
     except:
       return false      
