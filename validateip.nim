afc183f6a18e2c3ed10645ef257a9a2c181c1ff5
Akito <akito.kitsune@protonmail.com>
2019-06-26 13:31:34 +0200

    1st version finished Module should work now. Tests will follow.

--- validateip.nim
+++ validateip.nim
@@ -1,6 +1,22 @@
-import strutils
+import strutils, validateip/ip4
+export ip4
 
-type
-  ClassNick = enum
-    large, medium, small, multicast
-
+proc isValidIp4*(address: string, class: string): bool =
+  ## Wraps `validIp4` proc to provide more user friendly
+  ## parameter options. Here, you don't have to state the
+  ## class char, instead you can choose another common
+  ## alias for the corresponding class.
+  try:
+    case class.toLowerAscii
+    of "small", "local", "private", "lan", "c":
+      if validIp4(address, 'C'): return true
+    of "large", "internet", "public", "wan", "a":
+      if validIp4(address, 'A'): return true
+    of "medium", "b":
+      if validIp4(address, 'B'): return true
+    of "multicast", "d":
+      if validIp4(address, 'D'): return true
+    else:
+      "Invalid IPv4 address class.".quit
+  except ValueError, RangeError:
+    return false
